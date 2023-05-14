618aad9ffcedbbba493623e19e25bf993a834bd3
STQZ <16409691+STQZ@users.noreply.github.com>
2023-05-14 03:42:50 -0400

    Rework dark mode

--- src/extract/static/mhrice-dark.js
+++ src/extract/static/mhrice-dark.js
@@ -3,12 +3,18 @@ const darkModeToggle = document.querySelector("#dark-mode-toggle")
 
 
 const enableDarkMode = () => {
-    document.getElementById("stylething").setAttribute('href', "/mhrice-dark.css?h={}");
+    //document.getElementById("stylething").setAttribute('href', "/mhrice-dark.css?h={}");
+
+    document.documentElement.classList.add("darkmode");
+
     localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
-    document.getElementById("stylething").setAttribute('href', "/mhrice.css?h={}");
+    //document.getElementById("stylething").setAttribute('href', "/mhrice.css?h={}");
+
+    document.documentElement.classList.remove("darkmode");
+
     localStorage.setItem("darkMode", null);
 };
 
